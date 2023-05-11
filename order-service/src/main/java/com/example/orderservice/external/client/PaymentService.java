a09139aa5a91864bff8e1af6ebfc94fa0588a2d9
desaijay26 <desai26jay@gmail.com>
2023-05-11 18:29:45 +0530

    api gateway with circuit breaker

--- order-service/src/main/java/com/example/orderservice/external/client/PaymentService.java
+++ order-service/src/main/java/com/example/orderservice/external/client/PaymentService.java
@@ -4,7 +4,6 @@ import com.example.orderservice.exception.CustomException;
 import com.example.orderservice.external.request.PaymentRequest;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
